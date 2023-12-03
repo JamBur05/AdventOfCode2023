f219a6012a2d3af75fda20461a42bc8a7828faac
James Burge <james3906@gmail.com>
2023-12-03 00:37:01 +0000

    AOC Task1 Complete

---
+++ AOC1/AOC1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace AOC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "data.txt";
+            string line;
+            string currentNum = "";
+            string[] lineArr = new string[1000];
+            string[] combinedNums = new string[1000];
+            int i = 0;
+            int sum = 0;
+
+            Console.WriteLine("Hello, World!");
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lineArr[i] = line;
+                    i++;
+                }
+            }
+
+
+            for(i = 0; i < lineArr.Length; i++)
+            {
+                line = lineArr[i];
+                for(int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] >= 48 && line[j] <= 57)
+                    {
+                        currentNum = currentNum + line[j];
+                        break;
+                    }
+                }
+
+                for(int j = line.Length - 1; j >= 0; j--)
+                {
+                    if (line[j] >= 48 && line[j] <= 57)
+                    {
+                        currentNum = currentNum + line[j];
+                        break;
+                    }
+                }
+                combinedNums[i] = currentNum;
+                currentNum = "";
+            }
+
+            for(i = 0; i < combinedNums.Length; i++)
+            {
+                Console.WriteLine(combinedNums[i]);
+                sum = sum + (int.Parse(combinedNums[i]));
+            }
+            Console.WriteLine("Answer is {0}", sum);
+        }
+    }
+}
